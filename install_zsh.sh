788487cd08326cf66809b95cafd0a2c626b8437c
nc <mixtapeking@mail.ru>
2019-09-16 10:55:34 +0300

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -8,3 +8,6 @@ $SHELL --version
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo echo -e "alias gp=\"git pull\" \nalias tp=\"terraform plan\" \nalias taa=\"terraform apply -auto-approve\" \nalias tda=\"terraform destroy -auto-approve\"" >> ~/.zshrc
 . ~/.zshrc
+
+# change zsh theme
+sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
