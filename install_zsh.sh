c6ad92ccfa0e6a2948a91246471b55c953b7cdb0
nc <mixtapeking@mail.ru>
2019-09-16 09:30:45 +0300

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -6,3 +6,4 @@ chsh -s $(which zsh)
 echo $SHELL
 $SHELL --version
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo echo "alias gp="git pull"; alias tp="terraform plan"; alias taa="terraform apply -auto-approve"; alias tda="terraform destroy -auto-approve"" >> ~/.zshrc
